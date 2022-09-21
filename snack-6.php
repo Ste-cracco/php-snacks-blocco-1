7d972be237f3f24793769870ca4190eb0e4b7681
Ste-cracco <s.cracco@yahoo.it>
2022-09-21 16:56:56 +0200

    Snack 6 Completato

--- snack-6.php
+++ snack-6.php
@@ -27,9 +27,20 @@
 
 <div style=background-color:green >
     <?php
-        foreach($db as $item) {
-            echo $item['name'];;
+        echo 'Nomi insegnanti: ','<br>';
+        foreach($db['teachers'] as $teacher) {
+            echo $teacher['name'],' ';
+            echo $teacher['lastname'], '<br>';
         }
     ?>
+</div>
 
+<div style=background-color:lightgrey >
+    <?php
+        echo 'Nomi PM: ','<br>';
+        foreach($db['pm'] as $pm) {
+            echo $pm['name'],' ';
+            echo $pm['lastname'], '<br>';
+        }
+    ?>
 </div>
