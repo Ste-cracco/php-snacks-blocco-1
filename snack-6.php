e88160044c5f107aad4f7246b45d9989c3119fb8
Ste-cracco <s.cracco@yahoo.it>
2022-09-21 16:36:50 +0200

    Errore "Undefined index" riga 31

---
+++ snack-6.php
@@ -0,0 +1,35 @@
+<?php
+ 
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+ 
+?>
+
+<div style=background-color:green >
+    <?php
+        foreach($db as $item) {
+            echo $item['name'];;
+        }
+    ?>
+
+</div>
